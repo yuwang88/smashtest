124f13da1b2dc85f71c1e883ff4e0fcf7d6f5fbf
yuwang88 <yuwang6686@yahoo.co.jp>
2014-11-03 13:17:27 +0900

    init

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+	public float cameraspeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (0,0,cameraspeed*Time.deltaTime);
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log ("collision");
+//		animator.SetBool ();
+		transform.GetComponent<Animator> ().SetBool ("cameracontact",true);
+	}
+	public void AfterShake()
+	{
+		transform.GetComponent<Animator> ().SetBool ("cameracontact",false);
+	}
+}
---
+++ Assets/cameraeffect/Script/CameraMove.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+
+var speed : float = 0.01;
+
+function Update ()
+{
+	transform.Translate(Input.GetAxis("Horizontal")*speed,0,Input.GetAxis("Vertical")*speed);
+}
